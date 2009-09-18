ec67adf97eca2daf42706ab24814a8c170acfc83
James Coglan <jcoglan@googlemail.com>
2009-09-18 17:26:50 +0100

    Initial commit: new Helium project.

---
+++ source/map-navigation.js
@@ -0,0 +1,14 @@
+/**
+ * class MapNavigation
+ **/
+MapNavigation = new JS.Class('MapNavigation', {
+    extend: {
+        VERSION: '1.0.0'
+    },
+    
+    /**
+     * new MapNavigation()
+     **/
+    initialize: function() {}
+});
+
